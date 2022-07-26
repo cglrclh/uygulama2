245f79047eee25bb8d9c3b5b9da35406ba96a93b
cglrclh <caglarculha35@gmail.com>
2022-07-26 16:06:55 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace MathTask
+{
+    class MathTaskOne
+    {
+    
+        public static void Main()
+        {
+                        
+            double mealCost = Double.Parse(Console.ReadLine());
+            int tipPercent = Int32.Parse(Console.ReadLine());
+            int taxPercent = Int32.Parse(Console.ReadLine());
+            
+            double tip = mealCost * tipPercent / 100;
+            double tax = mealCost * taxPercent / 100;
+            
+            double totalCost = mealCost + tip + tax;
+            
+            Console.WriteLine("The total meal cost is " + Math.Round(totalCost) + " dollars.");
+            
+            
+        }
+        
+    }
+    
+}
